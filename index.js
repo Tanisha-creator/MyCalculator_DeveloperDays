2b268db9529c5090ada6e8098135bc47247867eb
Tanisha Kindo <tanishakindo@gmail.com>
2021-09-04 17:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-// select all the buttons
-
 const buttons = document.querySelectorAll("button");
-
-// select the <input type=”text” class=”display” disabled> element
-
 const display = document.querySelector(".display");
-
-// add eventListener to each button
-
 buttons.forEach(function(button) {
 
-  button.addEventListener("click", calculate);
+button.addEventListener("click", calculate);
 
 });
 
-// calculate function
-
 function calculate(event) {
-
-  // current clicked buttons value
 
   const clickedButtonValue = event.target.value;
 
   if (clickedButtonValue === "=") {
 
-    // check if the display is not empty then only do the calculation
-
     if (display.value !== "") {
-
-      // calculate and show the answer to display
 
       display.value = eval(display.value);
 
@@ -36,13 +20,9 @@ function calculate(event) {
 
   } else if (clickedButtonValue === "c") {
 
-    // clear everything on display
-
     display.value = "";
 
   } else {
-
-    // otherwise concatenate it to the display
 
     display.value += clickedButtonValue;
   }
